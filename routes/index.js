5dc29e22c948f55a725d4b46fa1e5ccaf70722b5
Alberto Pérez <apleon89@gmail.com>
2023-02-12 11:23:38 +0000

    lab completed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const MoviesModel = require('../models/Movie.model')
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+    
+    MoviesModel.find()
+    .then ( (response) => {
+        // console.log(response)
+        res.render('movies', {
+            allMovies: response
+        })
+    })
+    .catch ( (error) => {
+        next(error)
+    })
+})
+
+router.get('/movies/:movieId', async (req, res, next) => {
+
+    try {
+
+        const response = await MoviesModel.findById(req.params.movieId)
+        // console.log(response)
+        res.render('movie-info.hbs', {
+            movie: response
+        })
+
+    }
+    catch (error) {
+
+        next(error)
+    }
+
+
+
+
+})
 
 module.exports = router;
